f75361f64a92c510931ec0df06953e582b72e07d
unknown <tuannguyenm1@HSSSC1PCL01870.HarveyNash.vn.local>
2015-12-02 12:39:33 +0700

    Init

---
+++ QuanLyLich/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyLich.Startup))]
+namespace QuanLyLich
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
